8822d3738d6160c98515c822c78ea9e4dd4493c0
zhangkaizhou <zhangkaizhou@hotmail.com>
2017-05-23 17:21:01 +0800

    添加画三角形的代码

--- Lesson3/mindstorms.py
+++ Lesson3/mindstorms.py
@@ -5,6 +5,11 @@ def draw_square(some_turtle):
         some_turtle.forward(100)
         some_turtle.right(90)
 
+def draw_triangle(some_turtle):
+    for i in range(1, 4):
+        some_turtle.forward(100)
+        some_turtle.right(-120)
+
 def draw_art():    
     window = turtle.Screen()
     window.bgcolor("red")
@@ -19,6 +24,11 @@ def draw_art():
     angie.shape("arrow")
     angie.color("blue")
     angie.circle(100)
+    #Create the turtle Angie - Draws a triangle
+    trigle = turtle.Turtle()
+    trigle.shape("turtle")
+    trigle.color("yellow")
+    draw_triangle(trigle)
     
     window.exitonclick()
 
