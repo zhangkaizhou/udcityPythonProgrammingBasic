78d909fe9e746a7d158262b1568863612db7ce6c
zhangkaizhou <zhangkaizhou@hotmail.com>
2017-05-23 15:39:08 +0800

    改进代码练习

--- Lesson3/mindstorms.py
+++ Lesson3/mindstorms.py
@@ -1,23 +1,20 @@
 import turtle
 
-def draw_square():
+def draw_square(some_turtle):
+    for i in range(1, 5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_art():    
     window = turtle.Screen()
     window.bgcolor("red")
-    
+    #Create the turtle Brad - Draws a sqaure
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("yellow")
     brad.speed(2)
-    
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-
+    draw_square(brad)
+    #Create the turtle Angie - Draws a circle
     angie = turtle.Turtle()
     angie.shape("arrow")
     angie.color("blue")
@@ -26,4 +23,4 @@ def draw_square():
     window.exitonclick()
 
 
-draw_square()
+draw_art()
